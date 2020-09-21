6dd8d09d72995019ba0b0d2ed63b529b5d62f78e
Ashley Nash Chikomborero Chakanyuka <ashleychak@live.com>
2020-09-21 22:45:03 +0200

    lesson-5

--- header.php
+++ header.php
@@ -9,9 +9,10 @@
 <!DOCTYPE html>
 <html lang="<?php language_attributes(); ?>">
 <head>
-    <meta charset="<?php bloginfo('charset') ?>">
+    <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <?php wp_head(); ?>
 </head>
-<body>
+<body <?php body_class(); ?>>
+<?php wp_body_open(); ?>
 <header>Header</header>
