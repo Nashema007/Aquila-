9f23ca8c3cf2f570046dd9e87ade451e1df0e959
Ashley Nash Chikomborero Chakanyuka <ashleychak@live.com>
2020-09-21 22:55:49 +0200

    lesson-6

--- functions.php
+++ functions.php
@@ -5,3 +5,12 @@
  * 
  * @package Aquila
  */
+
+ function aquila_enqueue_scripts(){
+
+    wp_enqueue_style('stylesheet', get_stylesheet_uri(), [], filemtime(get_template_directory().'/style.css'),'all');
+ }
+
+add_action('wp_enqueue_scipts', 'aquila_enqueue_scripts');
+
+ ?>
